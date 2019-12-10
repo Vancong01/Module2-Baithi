4ca16797d8fb31f8022b0c4644265c824186f15a
Acnologia1 <46494938+Acnologia1@users.noreply.github.com>
2019-12-10 12:48:31 +0700

    1
    1

--- Module2-thi/src/main/java/com/codegym/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.Country;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CountryRepository extends PagingAndSortingRepository<Country,Long> {
-}
